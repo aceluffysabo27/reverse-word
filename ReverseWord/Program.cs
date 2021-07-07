6d96869f12e2f9e38f8040ff9ffb2f6c52a58a2a
DHELL <DHELL@DHELL>
2021-07-07 16:24:12 +0800

    First Commit

--- ReverseWord/Program.cs
+++ ReverseWord/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace ReverseWord
 {
+    class Worded
+    {
+
+        public void Reverse(string word)
+        {
+            string newWord = "";
+            for (int i = 0; i < word.Length; i++)
+            {
+                var x = word.Length - 1;
+                var temp = x - i;
+                newWord += word[temp];
+            }
+            Console.WriteLine("The new Reverse Word: \"{0}\"", newWord);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var worded = new Worded();
+
+            Console.WriteLine("Enter a Word: ");
+            var word1 = Console.ReadLine();
+
+            worded.Reverse(word1);
+            Console.Read();
         }
     }
 }
